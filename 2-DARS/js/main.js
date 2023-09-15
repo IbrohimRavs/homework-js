ad10f23dcb5115e60279ded8a6ae0a8b5e81a7f3
IbrohimRavs <124879713+IbrohimRavs@users.noreply.github.com>
2023-09-15 19:06:55 +0300

    Add files via upload

---
+++ 2-DARS/js/main.js
@@ -0,0 +1,280 @@
+// console.log("ok")
+// alert("ok")
+
+// let a = prompt("Name");
+// let b = prompt("Age");
+// let c = prompt("Gender");
+// let d = prompt("school");
+
+// console.log("Name", a);
+// console.log("Age", b);
+// console.log("Gender", c);
+// console.log("school", d);
+
+
+// let a =  prompt("Ismingizni kiriting");
+// if (a.length === 1) {
+//     throw new Error("ISMINGIZI XARIFDA KIRITING");
+// }
+// let b = prompt("Yoshingizi kiriting");
+// if (b.length < 1) {        
+//     throw new Error("XATO")
+// }
+// let c = prompt("6 + 3");
+// if (c.length < 1) {
+//     throw new Error("XATO")
+// }
+// let d = prompt("20 - 5");
+// if (d.length < 1) {
+//     throw new Error("XATO")
+// }
+// let x = prompt("15 * 2");
+// if (x.length < 1) {
+//     throw new Error("XATO")
+// }
+// let y = prompt("8 / 4");
+// if (y.length < 1) {
+//     throw new Error("XATO")
+// }
+// let j = prompt("10 % 3");
+// if (j.length < 1) {
+//     throw new Error("XATO")
+// }
+
+
+let savol__1 = prompt("1 jahon urushi qachon boshlangan");
+if(savol__1 == '1914') {
+    console.log('1-savolga javob togri');
+    
+}else{
+    console.log("1-savolga javob no togri");
+}
+if (savol__1.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+
+console.log("1914", "TOGRI JAVOB");
+
+let savol__2 = prompt("1 jahon urushi qachon tugagan");
+if (savol__2 == "1918"){
+    console.log("2-savolga javob togri")
+}else{
+    console.log("2-savolga javob no togri")
+}
+if (savol__2.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+
+console.log("1918", "TOGRI JAVOB");
+
+
+let savol__3 = prompt("2 jahon urushi qachon boshlangan");
+if (savol__3 = "1939"){
+    console.log('3-savolga javob togri')
+}else{
+    console.log("3-savolga javob no togri")
+}
+if (savol__3.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+
+console.log("1939", "TOGRI JAVOB");
+
+
+let savol__4 = prompt("2 jahon urushi qachon tugagan")
+if (savol__4 == "1945"){
+    console.log("4-savolga javob togri")
+}else{
+    console.log("4-savolga javob no togri")
+}
+if (savol__4.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+
+console.log("1945", "TOGRI JAVOB");
+
+
+let savol__5 = prompt("uchinchi reyx qachon sssr qarshi urush boshlagan")
+if (savol__5 == "1941"){
+    console.log("5-savolga javob togri");
+}else{
+    console.log("5-savolga javob notogri")
+}
+if (savol__5.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+
+console.log("1945", "TOGRI JAVOB");
+
+let savol__6 = prompt("O'zbekiston qaysi yili sssrdan chiqan")
+if (savol__6 == "1991") {
+    console.log("6-savolga javob togri");
+}else{
+    console.log("6-savolga javob no togri")
+}
+if (savol__6.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1941", "TOGRI JAVOB")
+
+let savol__7 = prompt("SSSR qaysi yilda tashkil topgan")
+if (savol__7 == "1922") {
+    console.log("7-savolga javob togri");
+}else{
+    console.log("7-savolga javob no togri")
+}
+if (savol__7.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1922", "TOGRI JAVOB")
+
+let savol__8 = prompt("SSSR qaysi yilgacham mavjud bogan")
+if (savol__8 == "1991") {
+    console.log("8-savolga javob togri");
+}else{
+    console.log("8-savolga javob no togri")
+}
+if (savol__8.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1991", "TOGRI JAVOB")
+
+let savol__9 = prompt("Javascript qaysi yili yaratilgan")
+if (savol__9 == "1995") {
+    console.log("9-savolga javob togri");
+}else{
+    console.log("9-savolga javob no togri")
+}
+if (savol__9.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1995", "TOGRI JAVOB")
+
+let savol__10 = prompt("Windows qaysi yili yaratilgan")
+if (savol__10 == "1985") {
+    console.log("10-savolga javob togri");
+}else{
+    console.log("10-savolga javob no togri")
+}
+if (savol__10.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1985", "TOGRI JAVOB")
+
+let savol__11 = prompt("Python qaysi yili yaratilgan")
+if (savol__11 == "1991") {
+    console.log("11-savolga javob togri");
+}else{
+    console.log("11-savolga javob no togri")
+}
+if (savol__11.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1991", "TOGRI JAVOB")
+
+let savol__12 = prompt("O'zbekistoni prezidenti ohirgi marta qachon almashgan")
+if (savol__12 == "2016") {
+    console.log("12-savolga javob togri");
+}else{
+    console.log("12-savolga javob no togri")
+}
+if (savol__12.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("2016", "TOGRI JAVOB")
+
+let savol__13 = prompt("O'zbekiston konstitutsiyasi qachon qabul qilingan")
+if (savol__13 == "1992") {
+    console.log("13-savolga javob togri");
+}else{
+    console.log("13-savolga javob no togri")
+}
+if (savol__13.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1992", "TOGRI JAVOB")
+
+let savol__14 = prompt("O'zbekiston konstitutsiyasi yangi qarorlari qachon qabul qilindi")
+if (savol__14 == "2023") {
+    console.log("14-savolga javob togri");
+}else{
+    console.log("14-savolga javob no togri")
+}
+if (savol__14.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("2023", "TOGRI JAVOB")
+
+let savol__15 = prompt("O'zbekistonda 10 ballik sistemasi maktabga qachon qiritilgan")
+if (savol__15 == "2023") {
+    console.log("15-savolga javob togri");
+}else{
+    console.log("15-savolga javob no togri")
+}
+if (savol__15.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("2023", "TOGRI JAVOB")
+
+let savol__16 = prompt("Apple kompaniyasi qachon tashkil topgan")
+if (savol__16 == "1976") {
+    console.log("16-savolga javob togri");
+}else{
+    console.log("16-savolga javob no togri")
+}
+if (savol__16.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1976", "TOGRI JAVOB")
+
+let savol__17 = prompt("Samsung kompaniyasi qachon tashkil topgan")
+if (savol__17 == "1969") {
+    console.log("17-savolga javob togri");
+}else{
+    console.log("17-savolga javob no togri")
+}
+if (savol__17.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1969", "TOGRI JAVOB")
+
+let savol__18 = prompt("Adolf Hitler qaysi yili tugilgan")
+if (savol__18 == "1889") {
+    console.log("18-savolga javob togri");
+}else{
+    console.log("18-savolga javob no togri")
+}
+if (savol__18.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1889", "TOGRI JAVOB")
+
+let savol__19 = prompt("NBA tashkil topgan yili")
+if (savol__19 == "1946") {
+    console.log("19-savolga javob togri");
+}else{
+    console.log("19-savolga javob no togri")
+}
+if (savol__19.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1946", "TOGRI JAVOB")
+
+let savol__20 = prompt("Futbol qaysi yili olimpiada oynlariga qiritilgan")
+if (savol__20 == "1900") {
+    console.log("20-savolga javob togri");
+}else{
+    console.log("20-savolga javob no togri")
+}
+if (savol__20.length > 4) {
+    throw new Error("BUNAQA YIL YOQ")
+}
+console.log("1900", "TOGRI JAVOB")
+
+// console.log("Isim",a);
+// console.log("Yosh",b);
+// console.log(6 + 3," togri javob",c,"sizni javobingiz");
+// console.log(20 - 5," togri javob",d,"sizni javobingiz");
+// console.log(15 * 2," togri javob",x,"sizni javobingiz");
+// console.log(8 / 4," togri javob",y,"sizni javobingiz");
+// console.log(10 % 3," togri javob",j,"sizni javobingiz");
